c33c999f5cde5acda6fc3beb926023b650fcec7f
markooni <arnel.maric@outlook.com>
2026-04-18 19:29:21 +0200

    minor update

--- app/src/main/java/com/example/stackoverflowapp/features/users/di/UserRepositoryModule.kt
+++ app/src/main/java/com/example/stackoverflowapp/features/users/di/UserRepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.example.stackoverflowapp.features.users.di
 
+import com.example.stackoverflowapp.features.users.data.repository.FollowRepositoryImpl
+import com.example.stackoverflowapp.features.users.data.repository.UserRepositoryImpl
+import com.example.stackoverflowapp.features.users.domain.repository.FollowRepository
 import com.example.stackoverflowapp.features.users.domain.repository.UserRepository
-import com.example.stackoverflowapp.features.users.remote.repository.UserRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -14,7 +16,9 @@ abstract class UserRepositoryModule {
 
     @Binds
     @Singleton
-    abstract fun bindUserRepository(
-        userRepositoryImpl: UserRepositoryImpl
-    ): UserRepository
+    abstract fun bindUserRepository(impl: UserRepositoryImpl): UserRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindFollowRepository(impl: FollowRepositoryImpl): FollowRepository
 }
